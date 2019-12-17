00c6cfae74b09b050d600208f636f166542a3298
weimo001 <308245304@qq.com>
2019-12-17 20:31:45 +0800

    first commit

---
+++ momohotel/src/main/java/com/xqr/momohotel/MomohotelApplication.java
@@ -0,0 +1,13 @@
+package com.xqr.momohotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomohotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomohotelApplication.class, args);
+	}
+
+}
---
+++ momohotel/src/test/java/com/xqr/momohotel/MomohotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xqr.momohotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomohotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
